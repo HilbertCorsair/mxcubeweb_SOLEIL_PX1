07024ab47edd57b673d35c468fe969efbd106c04
Dan Costin <dan-tiberiu.costin@synchrotron-soleil.fr>
2025-01-20 10:03:43 +0100

    Changes the IP Address and updates the CORS list. Also configures vite

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -2,12 +2,18 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
 
+
 export default defineConfig({
   plugins: [
     react(),
     { ...eslintPlugin(), apply: 'serve' }, // dev only to reduce build time
   ],
   server: {
+    host: "195.221.8.78",
+    port: 5173,
+    watch: {
+      usePolling: true  // This might help with some network setups
+    },
     open: true, // open default browser on start
     strictPort: true, // fail if port already in use (must be 5173 to match server's CORS config)
     proxy: {
