1c123c678a55e5c6f2c7b5c3c76da035b67a8dc7
Dan Costin <dan-tiberiu.costin@synchrotron-soleil.fr>
2025-07-03 11:42:27 +0200

    [Backup] MXCuBE Core avant integration du centrage automatique par murko, en cours de debug d'autoprocessing - Arthur F.

--- mxgo.sh
+++ mxgo.sh
@@ -1,5 +1,11 @@
 #!/bin/bash 
 #export PATH=/nfs/ruche/share-dev/px1dev/MXCuBE/mxcube_Dan/upgrade/mxcubeweb:$PATH
+
+export MURKO_PATH=/nfs/ruche/share-dev/px1dev/Arthur/murko-develop
+export MURKO_SIZEX=1024
+export MURKO_SIZEY=1360
+export MURKO_HOST=localhost
+export MURKO_PORT=89011
 export PATH=/nfs/ruche/share-dev/px1dev/MXCuBE/WebApp/mxcubeweb:$PATH
 #export PYTHONPATH=/nfs/ruche/share-dev/px1dev/MXCuBE/mxcube_Dan/upgrade/mxcubeweb:$PYTHONPATH
 export PYTHONPATH=/nfs/ruche/share-dev/px1dev/MXCuBE/WebApp/mxcubeweb:$PYTHONPATH
